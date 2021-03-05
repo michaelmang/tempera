3c79b167194f0eed1c99e6aa098197d721000246
Mike Mangialardi <mike.mangialardi@rackspace.com>
2021-03-05 13:26:04 -0500

    Generate messages on unofficial specs

--- packages/stylelint/src/rules/index.js
+++ packages/stylelint/src/rules/index.js
@@ -1,24 +1,43 @@
 import scorecard from '@tempera/postcss-scorecard';
 import { utils } from 'stylelint';
 
-import { namespace } from '../utils';
+import { namespace, validateSpecs } from '../utils';
 
-export const ruleName = namespace('unofficial');
+export const ruleName = namespace('official-specs');
 export const messages = utils.ruleMessages(ruleName, {
-  unofficial: ({ spec, nearestSpec }) => `Unofficial spec: "${spec}"`
+  'official-specs': ({ spec, nearestSpec }) => {
+    const prefix = `An unofficial spec was detected: "${spec}"`;
+    if (!nearestSpec) {
+      return prefix;
+    }
+
+    return `${prefix}. The nearest official spec is ${nearestSpec}`;
+  }
 });
 
 export default {
-  unofficial: function rule(enabled) {
+  'official-specs': function rule(specs = {}) {
     return (root, result) => {
       const validOptions = utils.validateOptions(result, ruleName, {
-        actual: enabled,
-        possible: [true, false]
+        actual: specs,
+        possible: [validateSpecs]
       });
   
       if (!validOptions) {
         return null;
       }
+
+      const { Once } = scorecard({
+        onInvalid: (score) => {
+          const spec = score.value;
+          const nearestSpec = score.nearestValue;
+          const node = score.context;
+
+          utils.report({ message: messages['official-specs']({ spec, nearestSpec }), node, result, ruleName })
+        },
+        specs,
+      });
+      Once(root);
     };
   }
 };
--- packages/stylelint/src/utils/index.js
+++ packages/stylelint/src/utils/index.js
@@ -1,4 +1,8 @@
-const prefix = 'tokens';
+const prefix = '@tempera';
 export function namespace(ruleName) {
 	return `${prefix}/${ruleName}`;
 };
+
+export function validateSpecs(specs) {
+	return Object.entries(specs).every(([_, value]) => typeof value !== "object");
+}
